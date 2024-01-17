3331c431a03e373974cb47bd0594f51f7d614a9e
Jeremy <jeremy.skrdlant@nwktc.edu>
2024-01-16 19:41:13 -0700

    Initial Commit

---
+++ NC3-Data-SwiftUI-Day3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NC3-Data-SwiftUI-Day3
+//
+//  Created by admin on 1/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NC3-Data-SwiftUI-Day3/NC3_Data_SwiftUI_Day3App.swift
@@ -0,0 +1,17 @@
+//
+//  NC3_Data_SwiftUI_Day3App.swift
+//  NC3-Data-SwiftUI-Day3
+//
+//  Created by admin on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct NC3_Data_SwiftUI_Day3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
